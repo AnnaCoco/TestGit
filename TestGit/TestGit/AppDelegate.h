7c2e9dadd44418d208ce6419396e1dc38ba3f250
AnnaCoco <xiaoxuesweet@yeah.net>
2016-08-23 23:33:36 +0800

    测试用的

---
+++ TestGit/TestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by 肖雪 on 16/8/23.
+//  Copyright © 2016年 Anna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGit/TestGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGit
+//
+//  Created by 肖雪 on 16/8/23.
+//  Copyright © 2016年 Anna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
